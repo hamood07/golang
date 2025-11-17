6c960bf78dea9eeeb1bcad4f5b24c403ec1b2dd7
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-11-17 19:43:38 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Replace this text with anything you want to display
+    text := "Hello, OpenShift ServiceAccount!"
+    
+    fmt.Println(text)
+}
